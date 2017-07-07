9017c946c2cc88a7046fd1730375bf6021aa3004
baranatmanoglu <baranatmanoglu@gmail.com>
2017-07-07 14:53:38 -0700

    Pair Game Added
    Pair Game Added

---
+++ PairGame/PairGame.pml
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<Package name="PairGame" format_version="4">
+    <Manifest src="manifest.xml" />
+    <BehaviorDescriptions>
+        <BehaviorDescription name="behavior" src="behavior_1" xar="behavior.xar" />
+    </BehaviorDescriptions>
+    <Dialogs>
+        <Dialog name="pair_game" src="pair_game/pair_game.dlg" />
+    </Dialogs>
+    <Resources>
+        <File name="style" src="html/css/style.css" />
+        <File name="1" src="html/images/1.jpg" />
+        <File name="2" src="html/images/2.jpg" />
+        <File name="3" src="html/images/3.jpg" />
+        <File name="4" src="html/images/4.jpg" />
+        <File name="5" src="html/images/5.jpg" />
+        <File name="6" src="html/images/6.jpg" />
+        <File name="7" src="html/images/7.jpg" />
+        <File name="8" src="html/images/8.jpg" />
+        <File name="is-bankasi-sym-trans" src="html/images/is-bankasi-sym-trans.png" />
+        <File name="index" src="html/index.html" />
+        <File name="jquery-ui.min" src="html/js/jquery-ui.min.js" />
+        <File name="jquery.flip" src="html/js/jquery.flip.js" />
+        <File name="matching" src="html/js/matching.js" />
+        <File name="tile" src="html/js/tile.js" />
+        <File name="main" src="main.py" />
+        <File name="baloons" src="html/images/baloons.png" />
+        <File name="jquery-2.1.4.min" src="html/js/jquery-2.1.4.min.js" />
+        <File name="qimessaging_helper" src="html/js/qimessaging_helper.js" />
+    </Resources>
+    <Topics>
+        <Topic name="pair_game_enu" src="pair_game/pair_game_enu.top" topicName="pair_game" language="en_US" />
+    </Topics>
+    <IgnoredPaths />
+    <Translations auto-fill="en_US">
+        <Translation name="translation_en_US" src="translations/translation_en_US.ts" language="en_US" />
+    </Translations>
+</Package>
---
+++ PairGame/html/js/matching.js
@@ -0,0 +1,196 @@
+var tiles = new Array(),
+    flips = new Array('tb', 'bt', 'lr', 'rl'),
+    iFlippedTile = null,
+    iTileBeingFlippedId = null,
+    tileImages = new Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10),
+    tileAllocation = null,
+    iTimer = 0,
+    iInterval = 100,
+    iPeekTime = 3000;
+
+var totalTiles = 16;
+var attempts = 0;
+var tilesFound = 0;
+
+function getRandomImageForTile() {
+
+    var iRandomImage = Math.floor((Math.random() * tileAllocation.length)),
+        iMaxImageUse = 2;
+
+    while (tileAllocation[iRandomImage] >= iMaxImageUse) {
+
+        iRandomImage = iRandomImage + 1;
+
+        if (iRandomImage >= tileAllocation.length) {
+
+            iRandomImage = 0;
+        }
+    }
+
+    return iRandomImage;
+}
+
+function createTile(iCounter) {
+
+    var curTile = new tile("tile" + iCounter),
+        iRandomImage = getRandomImageForTile();
+
+    tileAllocation[iRandomImage] = tileAllocation[iRandomImage] + 1;
+
+    curTile.setFrontColor("tileColor" + Math.floor((Math.random() * 5) + 1));
+    curTile.setStartAt(500 * Math.floor((Math.random() * 5) + 1));
+    curTile.setFlipMethod(flips[Math.floor((Math.random() * 3) + 1)]);
+    curTile.setBackContentImage("images/" + (iRandomImage + 1) + ".jpg");
+
+    return curTile;
+}
+
+function initState() {
+
+    /* Reset the tile allocation count array.  This
+    	is used to ensure each image is only 
+    	allocated twice.
+    */
+    tileAllocation = new Array(0, 0, 0, 0, 0, 0, 0, 0);
+
+    while (tiles.length > 0) {
+        tiles.pop();
+    }
+
+    $('#board').empty();
+    iTimer = 0;
+
+}
+
+function initTiles() {
+
+    var iCounter = 0,
+        curTile = null;
+
+    initState();
+
+    // Randomly create twenty tiles and render to board
+    for (iCounter = 0; iCounter < totalTiles; iCounter++) {
+
+        curTile = createTile(iCounter);
+
+        $('#board').append(curTile.getHTML());
+
+        tiles.push(curTile);
+    }
+}
+
+function hideTiles(callback) {
+
+    var iCounter = 0;
+
+    for (iCounter = 0; iCounter < tiles.length; iCounter++) {
+
+        tiles[iCounter].revertFlip();
+
+    }
+
+    callback();
+}
+
+function revealTiles(callback) {
+
+    var iCounter = 0,
+        bTileNotFlipped = false;
+
+    for (iCounter = 0; iCounter < tiles.length; iCounter++) {
+
+        if (tiles[iCounter].getFlipped() === false) {
+
+            if (iTimer > tiles[iCounter].getStartAt()) {
+                tiles[iCounter].flip();
+            } else {
+                bTileNotFlipped = true;
+            }
+        }
+    }
+
+    iTimer = iTimer + iInterval;
+
+    if (bTileNotFlipped === true) {
+        setTimeout("revealTiles(" + callback + ")", iInterval);
+    } else {
+        callback();
+    }
+}
+
+
+
+function checkMatch() {
+
+    if (iFlippedTile === null) {
+
+        iFlippedTile = iTileBeingFlippedId;
+
+    } else {
+        attempts++;
+        if (tiles[iFlippedTile].getBackContentImage() !== tiles[iTileBeingFlippedId].getBackContentImage()) {
+            //This is fail
+            setTimeout("tiles[" + iFlippedTile + "].revertFlip()", 2000);
+            setTimeout("tiles[" + iTileBeingFlippedId + "].revertFlip()", 2000);
+            session.raiseEvent("PairGame/PairNotFound", 1);
+
+        } else {
+            tilesFound += 2;
+            var eventVal = "N";
+            var tf2 = tilesFound / 2;
+            if (tf2 != 1) {
+                if (tf2 / attempts <= 1.2) {
+                    eventVal = "G"
+                }
+            }
+            if (totalTiles != tilesFound) {
+                session.raiseEvent("PairGame/PairFound", eventVal);
+            }
+        }
+
+        iFlippedTile = null;
+        iTileBeingFlippedId = null;
+    }
+    checkFinalState();
+}
+
+function checkFinalState() {
+    if (totalTiles == tilesFound) {
+        console.log("Mission accomplished: " + attempts);
+        session.raiseEvent("PairGame/GameFinished", attempts);
+    }
+}
+
+function onPeekComplete() {
+
+    $('div.tile').click(function () {
+
+        iTileBeingFlippedId = this.id.substring("tile".length);
+
+        if (tiles[iTileBeingFlippedId].getFlipped() === false) {
+            tiles[iTileBeingFlippedId].addFlipCompleteCallback(function () {
+                checkMatch();
+            });
+            tiles[iTileBeingFlippedId].flip();
+        }
+
+    });
+}
+
+function onPeekStart() {
+    setTimeout("hideTiles( function() { onPeekComplete(); })", iPeekTime);
+}
+
+
+
+
+$(document).ready(function () {
+
+    initTiles();
+    setTimeout("revealTiles(function() { onPeekStart(); })", iInterval);
+
+    //session.subscribeToEvent("PairGame/ShuffleCards", startGame);
+
+
+});
